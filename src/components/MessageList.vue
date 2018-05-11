a2b25b5fdc64922ec3e6d5aa9b3ec9d0e0f910f0
Michael Sitter <sitter.michael@gmail.com>
2018-05-10 19:42:49 -0700

    - messages store and message list component implementation

---
+++ src/components/MessageList.vue
@@ -0,0 +1,62 @@
+<template>
+<div class="message-container">
+  <div class="message-list">
+    <div v-for="(m, index) in messages.messages"
+      :class="{
+        'message-item': true,
+        'message-owner': m.name === user.name,
+      }"
+      :key="index">
+      <div class="message-text">
+        {{m.message}}
+      </div>
+      <div v-if="m.name !== user.name" class="message-user">
+        {{m.name}}
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  computed: mapState([
+    'user',
+    'messages',
+  ]),
+};
+</script>
+
+<style lang="scss">
+@import '../assets/base';
+
+.message-container {
+  display: flex;
+  flex-direction: column;
+}
+
+.message-item {
+  width: 50%;
+  padding: $unit;
+  text-align: left;
+
+  .message-text {
+    border-radius: $unit;
+    padding: $unit;
+    background-color: $white;
+  }
+
+  .message-user {
+    padding: $unit;
+  }
+
+  &.message-owner {
+    .message-text {
+      background-color: $primary;
+      color: $white;
+    }
+  }
+}
+</style>
---
+++ src/stores/messages.js
@@ -0,0 +1,35 @@
+import api from '../services/api';
+
+export default {
+  state() {
+    return {
+      messages: [],
+    };
+  },
+  mutations: {
+    addMessage(state, { name, message, reaction = null }) {
+      state.messages.push({
+        name,
+        message,
+        reaction,
+      });
+    },
+    setMessages(state, messages) {
+      state.messages = messages;
+    },
+  },
+  actions: {
+    async fetchMessages({ commit }, channelId) {
+      const resp = await api.getRoomMessages(channelId);
+      commit('setMessages', resp.body);
+      return resp.body;
+    },
+    async sendMessage({ commit }, {
+      channelId, name, message, reaction = null,
+    }) {
+      const resp = await api.getRoomMessages(channelId, { name, message, reaction });
+      commit('addMessage', { name, message, reaction });
+      return resp;
+    },
+  },
+};
